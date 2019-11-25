62e603556b508239aaaf1de935a0d44481ebf102
GSTARCZE <gabriel.starczewski@capgemini.com>
2019-11-25 13:26:36 +0100

    basic cucumber test

---
+++ src/test/java/com/mycompany/app/steps/Steps.java
@@ -0,0 +1,23 @@
+package com.mycompany.app.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Steps {
+
+	@Given("^I have first task$")
+	public void i_have_first_task() {
+		;
+	}
+
+	@When("^I attempt to solve it$")
+	public void i_attempt_to_solve_it() {
+		;
+	}
+
+	@Then("^I surely succeed$")
+	public void i_surely_succeed() {
+		;
+	}
+}
