187d0b1e23a61818c3722a9f15968152a71f2fab
GSTARCZE <gabriel.starczewski@capgemini.com>
2019-11-26 13:27:32 +0100

    fixing mvn for jenkins

--- src/test/java/com/mycompany/app/BaseRunnerTest.java
+++ src/test/java/com/mycompany/app/BaseRunnerTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","html:target/cucumber","json:target/cucumber.json"})
+@CucumberOptions(plugin = {"pretty","html:target/cucumber"})
 public class BaseRunnerTest {
 
 }
