7f374921f0b372faa77e157f66407f640966f5e2
GSTARCZE <gabriel.starczewski@capgemini.com>
2019-11-27 14:18:22 +0100

    addign hello tags

--- src/test/java/com/mycompany/app/BaseRunnerTest.java
+++ src/test/java/com/mycompany/app/BaseRunnerTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"json:target/cucumber.json"})
+@CucumberOptions(format = { "json:target/cucumber.json" }, tags = { "@hello" })
 public class BaseRunnerTest {
 
 }
--- src/test/resources/com/mycompany/app/basicScenario.feature
+++ src/test/resources/com/mycompany/app/basicScenario.feature
@@ -1,5 +1,6 @@
 Feature: First Feature
 
+  @hello
   Scenario: First Hello
     Given I have "first" task
     And Step from "First Hello" in "First Feature" feature file
